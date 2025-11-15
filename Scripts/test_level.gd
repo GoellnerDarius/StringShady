f909ddadfe2871601f5d8dce17dc0ee4c112244e
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-11-15 13:59:53 +0100

    Fixed tha out of bounds Bug

--- Scripts/test_level.gd
+++ Scripts/test_level.gd
@@ -9,22 +9,46 @@ const HUMAN_SCALE = Vector2(0.125, 0.125)
 @export var stringPoints: Array[Sprite2D]
 
 var currentUnderware:int
+var nextUnderWare:int
+var underwareHuman_map : Array[int] 
+
+
 
 
 func _ready():
+	for i in HumanSpawnPoints.size():
+		underwareHuman_map.append(0)
 	Spawnhumans(2)
+	SpawnUnderWare(FindAnUnderWare())
 	
 	
 func Spawnhumans(Amount):
 	for n in Amount:
-		var randomnumber: int = randi_range(0,3)
+		var randomnumber: int = randi_range(0,humanSprite.size()-1)
 		var child: TextureButton =  HumanSpawnPoints[n].get_child(0) 
 		child.texture_normal = humanSprite[randomnumber]
 		HumanSpawnPoints[n].scale = HUMAN_SCALE
-		
+		StringAssociate(n)
 
-func StringAssociate():
-	pass
+func StringAssociate(currentHuman):
+	var randomnumber:int = randi_range(0,stringSprite.size()-1)
+	underwareHuman_map[currentHuman] = randomnumber
+
+func FindAnUnderWare() -> int:
+	var randomnumber:int = randi_range(0,stringSprite.size()-1)
+	for n in underwareHuman_map:
+		if randomnumber == underwareHuman_map[n]:
+			return randomnumber
+	return FindAnUnderWare()
+	
+func SpawnUnderWare(UnderWare):
+	if stringPoints[0].texture == null:
+		nextUnderWare = FindAnUnderWare()
+		stringSprite[nextUnderWare]
+	currentUnderware = nextUnderWare
+	stringPoints[1].texture = stringSprite[currentUnderware]
+	stringPoints[0].texture = stringSprite[UnderWare]
+	nextUnderWare = UnderWare
 
 func _on_texture_button_button_up(extra_arg_0):
-	print(extra_arg_0)
+	print("Human"+ str(extra_arg_0) + "CurrentString"+ str(underwareHuman_map[extra_arg_0]))
