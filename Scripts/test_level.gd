21c561ea9d4321692a1ab903609eca91dbca9c0c
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-11-15 11:03:03 +0100

    started working on the mainSkript

--- Scripts/test_level.gd
+++ Scripts/test_level.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+
+@export var stringSprite : Array[Texture2D]
+@export var humanSprite : Array[Texture2D]
+
+@export var HumanSpawnPoints: Array[Node2D]
+@export var stringPoints: Array[Sprite2D]
