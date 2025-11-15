ff5d11e78d65d2929c90df2d52a85e6cab21d13d
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-11-15 17:31:07 +0100

    Added a different CharakterSystem

---
+++ Scripts/sprite_row.gd
@@ -0,0 +1,4 @@
+extends Node
+class_name SpriteRow
+
+@export var textures: Array[Texture2D]
--- Scripts/test_level.gd
+++ Scripts/test_level.gd
@@ -3,13 +3,26 @@ extends Node2D
 const HUMAN_SCALE = Vector2(0.125, 0.125)
 
 @export var stringSprite : Array[Texture2D]
-@export var humanSprite : Array[Texture2D]
+@export var humanSprite0 : Array[Texture2D] 
+@export var humanSprite1 : Array[Texture2D] 
+@export var humanSprite2 : Array[Texture2D] 
+@export var humanSprite3 : Array[Texture2D] 
+@export var humanSprite4 : Array[Texture2D] 
+@export var humanSprite5 : Array[Texture2D] 
+@export var humanSprite6 : Array[Texture2D] 
+@export var humanSprite7 : Array[Texture2D] 
+@export var humanSprite8 : Array[Texture2D] 
 
 @export var HumanSpawnPoints: Array[Control]
 @export var stringPoints: Array[Sprite2D]
 
+@export var stringButton:Array[Control]
+
+@onready var humanSprite = [humanSprite0,humanSprite1,humanSprite2,humanSprite3,humanSprite4,humanSprite5,humanSprite6,humanSprite7,humanSprite8]
+
 var currentUnderware:int
 var nextUnderWare:int
+var noUnderWareAmount:int
 var underwareHuman_map : Array[int] 
 
 
@@ -18,15 +31,16 @@ var underwareHuman_map : Array[int]
 func _ready():
 	for i in HumanSpawnPoints.size():
 		underwareHuman_map.append(0)
-	Spawnhumans(2)
+	Spawnhumans(7)
 	SpawnUnderWare(FindAnUnderWare())
 	
 	
 func Spawnhumans(Amount):
+	noUnderWareAmount += Amount 
 	for n in Amount:
-		var randomnumber: int = randi_range(0,humanSprite.size()-1)
+		var randomnumber: int = randi_range(0,1)
 		var child: TextureButton =  HumanSpawnPoints[n].get_child(0) 
-		child.texture_normal = humanSprite[randomnumber]
+		child.texture_normal = humanSprite[randomnumber][0]
 		HumanSpawnPoints[n].scale = HUMAN_SCALE
 		StringAssociate(n)
 
@@ -42,6 +56,11 @@ func FindAnUnderWare() -> int:
 	return FindAnUnderWare()
 	
 func SpawnUnderWare(UnderWare):
+	if noUnderWareAmount < 2:
+		if stringPoints[0].texture == null:
+			RoundWon()
+		else:
+			stringPoints[0].texture = null
 	if stringPoints[0].texture == null:
 		nextUnderWare = FindAnUnderWare()
 		stringSprite[nextUnderWare]
@@ -50,5 +69,12 @@ func SpawnUnderWare(UnderWare):
 	stringPoints[0].texture = stringSprite[UnderWare]
 	nextUnderWare = UnderWare
 
+func RoundWon():
+	pass
+
 func _on_texture_button_button_up(extra_arg_0):
 	print("Human"+ str(extra_arg_0) + "CurrentString"+ str(underwareHuman_map[extra_arg_0]))
+	if (underwareHuman_map[extra_arg_0] == currentUnderware):
+		print("correct")
+	else:
+		print("wrong")
