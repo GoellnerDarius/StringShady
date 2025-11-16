9ed3ea4bc7795faea9e0b966d1b8ab936cf57069
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-11-16 16:18:08 +0100

    added the start animations

--- Scripts/animation_player.gd
+++ Scripts/animation_player.gd
@@ -2,4 +2,9 @@ extends AnimationPlayer
 
 func _ready():
 	var anim:AnimationPlayer = self
+	anim.play("StartAnimation")
+
+
+func _on_animation_finished(anim_name):
+	var anim:AnimationPlayer = self
 	anim.play("Idle")
